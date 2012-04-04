3df279373366da9396f45098f5d12866e1185a92
James Strachan <james.strachan@gmail.com>
2012-04-04 16:09:10 +0100

    added a SimpleStream for easy pushing of events into a stream

---
+++ koolapp-reactive/src/main/kotlin/org/koolapp/reactive/SimpleStream.kt
@@ -0,0 +1,40 @@
+package org.koolapp.reactive
+
+import java.io.Closeable
+import java.util.ArrayList
+import java.util.Collection
+import java.util.concurrent.ConcurrentLinkedQueue
+
+/**
+ * A simple [[Stream]] that can have elements injected into its active [[Handler]] instances
+ */
+class SimpleStream<in T>(val handlers: Collection<Handler<T>> = ConcurrentLinkedQueue<Handler<T>>()): Stream<T>(), Handler<T> {
+
+    override fun open(handler: Handler<T>): Closeable {
+        handlers.add(handler)
+        return object: Closeable {
+            public override fun close() {
+                handlers.remove(handler)
+            }
+        }
+    }
+
+    override fun onComplete(): Unit {
+        forEach{ it.onComplete() }
+    }
+
+    override fun onError(e: Exception): Unit {
+        forEach{ it.onError(e) }
+    }
+
+    override fun onNext(next: T): Unit {
+        forEach{ it.onNext(next) }
+    }
+
+    protected fun forEach(fn: (Handler<T>) -> Any): Unit {
+        for (handler in handlers) {
+            (fn)(handler)
+        }
+    }
+
+}
