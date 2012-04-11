84a10424c024508a759b579e841fce052a7e8357
James Strachan <james.strachan@gmail.com>
2012-04-11 08:56:25 +0100

    added test case for KT-1761

---
+++ koolapp-stream-math/src/test/kotlin/test/koolapp/stream/math/CalculationsTest.kt
@@ -0,0 +1,35 @@
+package test.koolapp.stream.math
+
+import kotlin.test.*
+
+import org.koolapp.stream.*
+import org.koolapp.stream.math.*
+
+import org.junit.Test as test
+import java.util.List
+
+class CalculationsTest {
+
+    test fun calculationsOnDoubles() {
+        val numbers = arrayList(1.0, 2.0, 3.0)
+        val v = numbers.variance()
+
+        println("variance of $numbers is $v")
+
+        assertTrue(v > 0)
+    }
+
+    /**
+    TODO compiler issue
+    see: http://youtrack.jetbrains.com/issue/KT-1761
+
+    test fun calculationsOnInts() {
+        val numbers = arrayList(1, 2, 3)
+        val v = numbers.variance()
+
+        println("variance of $numbers is $v")
+
+        assertTrue(v > 0)
+    }
+    */
+}
