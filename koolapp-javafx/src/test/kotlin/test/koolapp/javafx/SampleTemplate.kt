f62000903d75d138eaf042794ed728182262bed0
James Strachan <james.strachan@gmail.com>
2012-05-22 09:59:23 +0100

    refactored the code to separate out the 'could be JS' code one day from the 'definitely JavaFX code' ;)

--- koolapp-javafx/src/test/kotlin/test/koolapp/javafx/SampleTemplate.kt
+++ koolapp-javafx/src/test/kotlin/test/koolapp/javafx/SampleTemplate.kt
@@ -1,12 +1,22 @@
-package test.koolapp.javafx.templates
+package test.koolapp.javafx
 
 import org.koolapp.template.html.*
 import org.w3c.dom.Document
 import org.w3c.dom.Node
 import kotlin.dom.*
 
-fun sampleTemplate(document: Node): Node {
+fun myApp(document: Document) {
+    val newNode = sampleTemplate(document)
+    println("About to insert DOM node $newNode")
+    val container = document.getElementById("view")
+    if (container != null) {
+        container.appendChild(newNode)
+        println("Added new node!!!")
+    } else println("Cannot find container!")
+}
+
+fun sampleTemplate(document: Document): Node {
     return document.div {
-        h1("Template Generated Content!")
+        h1("Kool Template Generated Content!")
     }
 }
--- koolapp-javafx/src/test/kotlin/test/koolapp/javafx/WebViewSample.kt
+++ koolapp-javafx/src/test/kotlin/test/koolapp/javafx/WebViewSample.kt
@@ -1,21 +1,13 @@
 package test.koolapp.javafx
 
 import org.koolapp.javafx.WebApplication
-import test.koolapp.javafx.templates.sampleTemplate
 
 public class WebViewSample(): WebApplication() {
 
     override fun loadInitial(): String {
         ready {
-            val newNode = sampleTemplate(it)
-            println("About to insert DOM node $newNode")
-            val container = it.getElementById("view")
-            if (container != null) {
-                container.appendChild(newNode)
-                println("Added new node!!!")
-            } else println("Cannot find container!")
+            myApp(it)
         }
-
         return load("file://src/test/resources/sample.html")
     }
 }
