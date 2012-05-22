a702b43bdd364543bc3c12f62813b12bb34e9278
James Strachan <james.strachan@gmail.com>
2012-05-22 14:49:21 +0100

    added dummy test cases to fix the CI build

---
+++ koolapp-html-modelgen/src/test/kotlin/test/koolapp/htmlgen/Dummy.kt
@@ -0,0 +1,8 @@
+package test.koolapp.htmlgen
+
+import org.junit.Test as test
+
+class Dummy {
+    test fun dummy() {
+    }
+}
---
+++ koolapp-html/src/test/kotlin/test/koolapp/html/Dummy.kt
@@ -0,0 +1,8 @@
+package test.koolapp.html
+
+import org.junit.Test as test
+
+class Dummy {
+    test fun dummy() {
+    }
+}
