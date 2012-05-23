2c4b205bfc954b4e99064e737fcc598843792a38
James Strachan <james.strachan@gmail.com>
2012-05-23 11:20:10 +0100

    added full HTML5 support to the template engine; generating the elements and attributes from the HTML5 specification so we're typesafe and complete

--- koolapp-template/src/main/kotlin/org/koolapp/template/html/HtmlTemplates.kt
+++ koolapp-template/src/main/kotlin/org/koolapp/template/html/HtmlTemplates.kt
@@ -35,6 +35,20 @@ fun Node.textElement(localName: String, text: String? = null, init: Element.()->
     return answer
 }
 
+
+
+fun Node.h2(text: String? = null, init: Element.()-> Unit): Element = textElement("h2", text, init)
+
+fun Node.h3(text: String? = null, init: Element.()-> Unit): Element = textElement("h3", text, init)
+
+fun Node.h4(text: String? = null, init: Element.()-> Unit): Element = textElement("h4", text, init)
+
+
+
+
+/*
+fun Node.p(init: Element.()-> Unit): Element = element("p", init)
+
 fun Node.html(init: Element.()-> Unit): Element = element("html", init)
 
 fun Node.body(init: Element.()-> Unit): Element = element("body", init)
@@ -43,18 +57,6 @@ fun Node.div(init: Element.()-> Unit): Element = element("div", init)
 
 fun Node.h1(text: String? = null): Element = h1(text) {}
 
-fun Node.h1(text: String? = null, init: Element.()-> Unit): Element = textElement("h1", text, init)
-
-fun Node.h2(text: String? = null, init: Element.()-> Unit): Element = textElement("h2", text, init)
-
-fun Node.h3(text: String? = null, init: Element.()-> Unit): Element = textElement("h3", text, init)
-
-fun Node.h4(text: String? = null, init: Element.()-> Unit): Element = textElement("h4", text, init)
-
-fun Node.p(init: Element.()-> Unit): Element = element("p", init)
-
-fun Node.a(href: String? = null, title: String? = null, text: String? = null): Element = a(href, title, text) {}
-
 fun Node.a(href: String? = null, title: String? = null, text: String? = null, init: Element.()-> Unit): Element {
     val answer = textElement("a", text, init)
     if (href != null) answer.setAttribute("href", href)
@@ -62,6 +64,9 @@ fun Node.a(href: String? = null, title: String? = null, text: String? = null, in
     return answer
 }
 
+fun Node.h1(text: String? = null, init: Element.()-> Unit): Element = textElement("h1", text, init)
+
+fun Node.a(href: String? = null, title: String? = null, text: String? = null): Element = a(href, title, text) {}
 
 fun Node.img(src: String? = null, alt: String? = null): Element = img(src, alt) {}
 
@@ -73,5 +78,7 @@ fun Node.img(src: String? = null, alt: String? = null, init: Element.()-> Unit):
     answer.init()
     return answer
 }
+*/
+
 
 
